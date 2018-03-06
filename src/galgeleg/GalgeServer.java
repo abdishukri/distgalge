4a28f4e8f384949d341709ab352aefa34b24da8c
dabato <faras85@hotmail.com>
2018-03-06 12:13:26 +0100

    galgeserver

--- src/galgeleg/GalgeServer.java
+++ src/galgeleg/GalgeServer.java
@@ -1,7 +1,31 @@
 package galgeleg;
 
+import java.net.MalformedURLException;
+import java.net.NotBoundException;
+import java.net.RemoteException;
+import javax.xml.ws.Endpoint;
+
 public class GalgeServer {
 
-    public GalgeServer() {}
+    public static void Main(String[] arg)throws RemoteException, MalformedURLException, NotBoundException{
+
+        Galgelogik spil = new Galgelogik();
+
+        try {
+            spil.hentOrdFraDr();
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+
+        }
+        Endpoint.publish(""http://[::]:8080/galgele", spil);
+
+        System.out.println("starter server!")
+
+                spil.reset();
+    }
+
 
 }
